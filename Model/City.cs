9f4b0a13409e2e90278c85626596eb8e56db0a74
duyvm <duyvm@tahospital.vn>
2025-05-11 17:08:26 +0700

    Initial first commit

---
+++ Model/City.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CitiesManager.WebApi.Model
+{
+    public class City
+    {
+        [Key]
+        public Guid CityID { get; set; }
+        [Required(ErrorMessage = "City Name can't be blank")]
+        public string? CityName { get; set; }
+    }
+}
